8adcbd6d5514d2917bbad284791dd0a0cd2d4590
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-04-24 16:47:39 +0900

    Initial Commit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by 山本ののか on 2020/04/24.
+//  Copyright © 2020 Nonoka Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
